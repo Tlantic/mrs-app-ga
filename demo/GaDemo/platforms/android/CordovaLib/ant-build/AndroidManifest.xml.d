c3d39517b74bb79d0bfe4e95af1023187a65667c
Pedro Simeão Carvalho <pesimeao@gmail.com>
2015-06-16 12:16:27 +0100

    <feat> remove Demo app (deprecated)

--- demo/GaDemo/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\github\tlantic\tlanjs-ga\demo\GaDemo\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-D:\github\tlantic\tlanjs-ga\demo\GaDemo\platforms\android\CordovaLib\AndroidManifest.xml \
